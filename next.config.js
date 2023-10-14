1671a7e8931a67a309021bee4fb0b317a26d9018
Achal <achaldev3@gmail.com>
2023-10-14 17:40:07 +0530

    updated config file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: "export",
+  basePath: "/rest-countries-api",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
